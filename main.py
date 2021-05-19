c5cd660b6e7feb9b1d1fcd51ff928e86bc2c3229
manish <manishdarmal@gmail.com>
2021-05-19 23:33:04 +0700

    Add gunicorn

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
 from flask_gravatar import Gravatar
 from functools import wraps
+import os
 
 # Importing forms.
 from forms import RegisterForm, CreatePostForm, LoginForm, CommentForm
@@ -25,7 +26,7 @@ def admin_only(f):
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 
 ckeditor = CKEditor(app)
 
@@ -53,7 +54,7 @@ def load_user(user_id):
 
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
